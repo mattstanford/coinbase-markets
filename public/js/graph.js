11bca1981ba5572433fe2b091cb3fa25da502bca
Matt Stanford <mds6058@gmail.com>
2013-12-05 22:44:22 -0500

    Initial D3 graph with sample data

--- public/js/graph.js
+++ public/js/graph.js
@@ -9,14 +9,60 @@
 	};
 	
 	//This will eventually become a callback funciton
-	function coinbaseDataReceived(data) {
+	function coinbaseDataReceived(server_data) {
 		
-		drawGraph(data);
+		drawGraph(server_data);
 		
 	}
 	
-	function drawGraph(data) {
+	function drawGraph(server_data) {
 		
+		var graph = d3.select("#coinbase-graph");
+		var graphHeight = $("#coinbase-graph").height();
+		var graphWidth = $("#coinbase-graph").width();
+		var graphMargin = 40;	
+		var y_scale = getYScale(server_data, graphHeight, graphMargin);	
+		var x_scale = getXScale(server_data, graphWidth, graphMargin);
+	
+		drawGraph_data(graph, server_data, x_scale, y_scale);
+
+	}
+	
+	function drawGraph_data(graph, server_data, x_scale, y_scale)
+	{
+			graph.selectAll("circle")
+				.data(server_data)
+				.enter().append("circle")
+				.attr("cy", function(d) { return y_scale(d); })
+				.attr("cx", function(d,i) { return x_scale(i); })
+				.attr("r", 8)
+				.attr("fill", "red");
+	}
+	
+function getYScale(server_data, graphHeight, graphMargin) {
+		
+		var max_value = Math.max.apply(Math, server_data);
+		
+		//Give the max value a little bit of a buffer
+		max_value = max_value + (10 - (max_value % 10));
+		
+		var y_scale = d3.scale.linear()
+			.domain([0, max_value])
+			.range([0 + graphMargin, graphHeight - graphMargin]);
+		
+		return y_scale;
+		
+	}
+	
+	function getXScale(server_data, graphWidth, graphMargin) {
+		
+		var max_value = server_data.length;
+		
+		var x_scale = d3.scale.linear()
+		.domain([max_value,0])
+		.range([0 + graphMargin, graphWidth - graphMargin]);
+		
+		return x_scale;
 	}
 	
 })();
