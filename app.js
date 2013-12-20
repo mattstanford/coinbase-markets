8e9c497013aaf0318877a5f72fe6f9ea211ae190
Matt Stanford <mds6058@gmail.com>
2013-12-20 14:58:46 -0500

    Made sure the site can be accessed outside of the dev environment

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ require('./controllers/coinbaseDataPipe.js')(graphData);
 require('./controllers/index')(app, graphData);
 
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), "0.0.0.0", function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
