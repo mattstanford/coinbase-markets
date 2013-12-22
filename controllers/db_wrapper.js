488f7faa25cf0cc5cbc753b6db011abf585e9c7f
Matt Stanford <mds6058@gmail.com>
2013-12-22 17:24:06 -0500

    Updated db wrapper so user can set separate functions for success and error

--- controllers/db_wrapper.js
+++ controllers/db_wrapper.js
@@ -5,12 +5,19 @@
 ***/
 module.exports = {
 
-  query: function(pg_instance, text, callback) {
+  query: function(pg_instance, text, success_cb, error_cb) {
 
     pg_instance.connect(function(err, client, done) {
         
 	  client.query(text, function(err, result) {
-          callback(err, result);
+		  
+		  if (err) {
+			  error_cb(err, result);
+		  } 
+		  else {
+			  success_cb(result);
+		  }
+          
           pg_instance.end();
 	  });
 	  
