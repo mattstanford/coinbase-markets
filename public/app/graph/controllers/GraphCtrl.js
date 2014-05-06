37d6737317b7c99972cdf71ab08152bffb419daa
Matt Stanford <mds6058@yahoo.com>
2014-05-05 22:43:05 -0400

    Hooked up test directive

--- public/app/graph/controllers/GraphCtrl.js
+++ public/app/graph/controllers/GraphCtrl.js
@@ -1,12 +1,17 @@
 
 angular.module('coinbaseMarkets').controller('GraphCtrl', function ($scope) 
 {
+	$scope.testVar = 0;
+	
 	$scope.onBuyClick = function() {
         console.log("Buy");
+        $scope.testVar = 1;
+        
     }
 	
 	$scope.onSellClick = function() {
         console.log("Sell");
+        $scope.testVar = 2;
     }
  
 });
--- public/app/graph/directives/graphDirective.js
+++ public/app/graph/directives/graphDirective.js
@@ -1,5 +1,13 @@
 
 angular.module('coinbaseMarkets').directive('cbmGraph', function () 
 {
-  
+	return function ($scope, element, attrs) 
+	{
+		console.log("directive doing stuff");
+		
+		$scope.$watch('testVar', function() {
+			console.log("testVar changed!");
+		})
+		
+	}
 });
