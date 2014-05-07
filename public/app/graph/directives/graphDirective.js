0228febd1602bbe7e4cc67f381d8be5d021615ce
Matt Stanford <mds6058@yahoo.com>
2014-05-06 23:55:27 -0400

    Set the scope in the directive correctly

--- public/app/graph/directives/graphDirective.js
+++ public/app/graph/directives/graphDirective.js
@@ -1,13 +1,16 @@
 
 angular.module('coinbaseMarkets').directive('cbmGraph', function () 
 {
-	return function ($scope, element, attrs) 
-	{
-		console.log("directive doing stuff");
-		
-		$scope.$watch('testVar', function() {
-			console.log("testVar changed!");
-		})
-		
-	}
+	 return {
+		 restrict: 'E',
+		 scope: true,
+		 link: function (scope, element) {
+
+			console.log("directive doing stuff");
+			
+			scope.$watch('testVar', function() {
+				console.log("testVar changed!");
+			});
+		}
+	 }
 });
