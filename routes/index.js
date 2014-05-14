26d0b471dcb54eb9b390193c7b39402fea8c88f0
Matt Stanford <mds6058@yahoo.com>
2014-05-13 22:02:25 -0400

    Using Jade templates for views now

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
 
 exports.index = function(req, res){
 	//Temporarily just using plain HTML file for now
-	//res.render('index', { title: 'Coinbase Markets' });
+	res.render('index', { title: 'Coinbase Markets' });
 	
-	res.render('index.html');
+	//res.render('index.html');
 };
